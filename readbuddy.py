c1616082fd853fa8ef56fc1ff8826ef0b37c5cf8
BANADDA MUBARAKA <83466862+BANADDA@users.noreply.github.com>
2025-01-16 18:04:54 +0300

    modified:   readbuddy.py 	modified:   src/server.py

--- readbuddy.py
+++ readbuddy.py
@@ -1,5 +1,7 @@
-# ./readbuddy.py
 from src.server import app
+
+# Export the app variable for Gunicorn
+application = app  
 
 if __name__ == '__main__':
     app.run()
--- src/server.py
+++ src/server.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
 
@@ -12,4 +14,6 @@ CORS(app)
 init_routes(app)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # Get port from environment variable or default to 10000
+    port = int(os.environ.get('PORT', 10000))
+    app.run(debug=True, host='0.0.0.0', port=port)
